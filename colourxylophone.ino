28accdda51e14e6b4c261cdd6c39a27031c2c744
Mithilesh Domala <mithileshd27@users.noreply.github.com>
2016-04-06 18:22:52 +0530

    Update colourxylophone.ino

--- colourxylophone.ino
+++ colourxylophone.ino
@@ -23,13 +23,19 @@ tunned for most of the colors
 ******************************************************************************************/
 #include <Adafruit_NeoPixel.h>
 #include <avr/power.h>
+#include <SoftwareSerial.h>
+
+SoftwareSerial bluetooth(3,4);
 //#define wsled 6
 //Adafruit_NeoPixel ring = Adafruit_NeoPixel(16, wsled, NEO_GRB + NEO_KHZ800);
 int s2 = 10;  //color sensor
 int s3 = 11;  //color sensor
 int out = 2;  //color sensor
-int s0 = 3;
-int s1 = 4;
+int s0 = 9;
+int s1 = 8;
+int ledstate = 7;
+
+int state = 0; // 1 - red, 2 - green, 3 - blue, 0 - ignore
 
 unsigned long int pulseWidth;
 
@@ -37,8 +43,29 @@ int rColor = 0;
 int gColor = 0;
 int bColor = 0;
 
+
+int getcolourstate(){
+  if(rColor>230 && rColor<255 &&  gColor>0 && gColor<25 && bColor>=0 && bColor<20 ){
+    return 1;
+  }
+  if(rColor>95 && rColor<125 &&  gColor>200 && gColor<230 && bColor>=0 && bColor<20 ){
+    return 2;
+  }
+  if(rColor>5 && rColor<30 &&  gColor>90 && gColor<110 && bColor>155 && bColor<175 ){
+    return 3;
+  }
+  if(rColor>60 && rColor<80 &&  gColor>0 && gColor<30 && bColor>=0 && bColor<25 ){
+    return 4;
+  }
+  return 0;
+  /*if(rColor>10 && rColor<30 &&  gColor>110 && gColor<125 && bColor>175 && bColor<190 ){
+    state = 5;
+  }*/
+}
+
 void setup() 
 {
+ bluetooth.begin(115200);
  Serial.begin(9600);
  //ring.begin();
  //ring.show();
@@ -49,18 +76,15 @@ void setup()
  pinMode(out,INPUT);
  pinMode(s0, OUTPUT);
  pinMode(s1, OUTPUT);
-
- pinMode(5, OUTPUT);
- pinMode(6, OUTPUT);
- pinMode(9, OUTPUT);
-
+ pinMode(ledstate, OUTPUT);
+ 
  digitalWrite(s0, LOW);
  digitalWrite(s1, HIGH);
+ digitalWrite(ledstate, HIGH);
 }
 
 void loop() 
 {
-
   //read red color
    digitalWrite(s2,LOW);
    digitalWrite(s3,LOW);
@@ -76,7 +100,7 @@ void loop()
    gColor = (255 -gColor);
 
    //read blue color
-  digitalWrite(s2,LOW);
+   digitalWrite(s2,LOW);
    digitalWrite(s3,HIGH);
    pulseWidth=pulseIn(out,LOW);
    bColor = (pulseWidth/400) - 1;
@@ -160,25 +184,32 @@ bColor=0;
 bColor =bColor * .06;
 gColor =gColor * .95;
 //rColor =rColor * .002; 
-   Serial.print(rColor);
+   /*Serial.print(rColor);
    Serial.print(",");
    Serial.print(gColor);
    Serial.print(",");
-   Serial.println(bColor);
+   Serial.println(bColor);*/
    
    //delay(100);
-   analogWrite(5, rColor);
-   analogWrite(9, gColor);
-   analogWrite(6, bColor);
-   
-//ADD ANY RGB LED CODE in this case it is neopixel
-
-  /*for(int i=0;i<=16;i++)
-  {  // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
-    ring.setPixelColor(i, ring.Color(rColor,gColor,bColor)); 
-    ring.show(); // This sends the updated pixel color to the hardware.
-    delay(50);
-  } */
-
+   state = getcolourstate();
+   Serial.print("R = ");
+   Serial.println(rColor);
+   Serial.print("G = ");
+   Serial.println(gColor);
+   Serial.print("B = ");
+   Serial.println(bColor);
+   switch(state){
+    case 0 : bluetooth.write(0x30);
+             break;
+    case 1 : bluetooth.write(0x31);
+             break;
+    case 2 : bluetooth.write(0x32);
+             break;
+    case 3 : bluetooth.write(0x33);
+             break;
+    case 4 : bluetooth.write(0x34);
+             break;
+    default : break;
+   }
+   delay(500);
 }
-
