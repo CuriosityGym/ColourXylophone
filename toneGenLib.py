7e8012ae8b2f799b83e1a851cc900781532cbf6e
Rupin <rupin.chheda@gmail.com>
2016-04-07 17:40:13 +0530

    tempcode

---
+++ toneGenLib.py
@@ -0,0 +1,18 @@
+
+
+duration=0.3
+freq=300
+samplingRate=44100
+def sine(frequency, length, rate):
+    length = int(length * rate)
+    factor = float(frequency) * (math.pi * 2) / rate
+    return numpy.sin(numpy.arange(length) * factor)
+
+
+def play_tone(stream, frequency=freq, length=duration, rate=samplingRate):
+    chunks = []
+    chunks.append(sine(frequency, length, rate))
+
+    chunk = numpy.concatenate(chunks) * 0.25
+
+    stream.write(chunk.astype(numpy.float32).tostring())
